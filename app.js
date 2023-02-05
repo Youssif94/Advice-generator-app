d763a46a9250dc02b031e44cbddbc829ef69dd89
Youssif94 <86874043+Youssif94@users.noreply.github.com>
2023-02-06 00:06:25 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// Selectors
+const adviceNo = document.querySelector('.advise-no')
+const adviceHTML = document.querySelector('.advice')
+const dice = document.querySelector('.fa-dice-five')
+
+addEventListener("load", getAdvice)
+
+dice.addEventListener('click',getAdvice)
+// api
+
+let id,
+    advice;
+async function getAdvice(){
+    const res = await (await fetch('https://api.adviceslip.com/advice')).json()
+    id = res.slip.id
+    advice = res.slip.advice
+    adviceNo.innerHTML = id
+    adviceHTML.innerHTML = `"${advice}"`
+}
+
